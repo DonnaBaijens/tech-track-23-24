56ab05d044ab595f12268cfd88100ff467f35055
DonnaBaijens <donna.baijens@hva.nl>
2023-10-31 15:09:10 +0100

    Svelte installation

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,14 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+	import TestBarChart from '../components/test.svelte';
+</script>
+
+<TestBarChart />
+	
+<svelte:head>
+	<title>Tech Track Boilerplate</title>
+	<link rel="icon" href="/images/favicon.png" type="image/png" />
+	<link rel="stylesheet" href="generalstyle.css" />
+</svelte:head>
